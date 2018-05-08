d6ca63212bfb4336902e2f8c7a8cd1baa9b3e318
vipinv95 <35180085+vipinv95@users.noreply.github.com>
2018-05-08 13:44:22 -0700

    Add files via upload

---
+++ BoostingAlgorithms/decision_stump.py
@@ -0,0 +1,16 @@
+import numpy as np
+from typing import List
+from classifier import Classifier
+
+class DecisionStump(Classifier):
+	def __init__(self, s:int, b:float, d:int):
+		self.clf_name = "Decision_stump"
+		self.s = s
+		self.b = b
+		self.d = d
+
+	def train(self, features: List[List[float]], labels: List[int]):
+		pass
+		
+	def predict(self, features: List[List[float]]) -> List[int]:
+		return [self.s if f[self.d] > self.b else -1*self.s for f in features]
